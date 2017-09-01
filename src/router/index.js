19f205fd017aaa01761567d2fb650631c40fb08f
Anoop Sankar <anoop.sankar@gmail.com>
2017-09-01 13:06:52 +0530

    Cleanup boilerplate from the template.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import UserList from '@/components/UserList'
 import UserDetails from '@/components/UserDetails'
 
@@ -12,6 +11,5 @@ export default new Router({
     { name:'home', path: '/',         component: UserList },
     { name:'user_new', path: '/user/new', component: UserDetails, props: { isNewUser: true} },
     { name:'user_edit', path: '/user/:id', component: UserDetails, props: true },
-    { name:'helo', path: '/hello',    component: Hello },
   ]
 })
